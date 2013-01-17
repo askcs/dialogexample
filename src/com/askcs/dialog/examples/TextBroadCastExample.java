7fff311354780790c011ded5e239cc59f85c66ab
Sven Stam <sstam@ask-cs.com>
2013-01-17 14:33:20 +0100

    Broadcast text message example

---
+++ src/com/askcs/dialog/examples/TextBroadCastExample.java
@@ -0,0 +1,44 @@
+package com.askcs.dialog.examples;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.askcs.dialog.sdk.QuestionBuilder;
+import com.askcs.dialog.sdk.model.Question;
+
+public class TextBroadCastExample extends HttpServlet {
+
+	private static final long serialVersionUID = 900693350795921277L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		String servletURL = req.getRequestURL().toString().replace(req.getRequestURI(), req.getServletPath());
+		String reqURL = req.getRequestURI().substring(1);
+		String[] parts = reqURL.trim().split("/");
+		String result = "{}";
+		String message = req.getParameter("message");
+		
+		if(message!=null) {
+			String questionNo = "1"; // Set a question number. This is to point from one question to another
+			String type = Question.QUESTION_TYPE_COMMENT; // Type referral will send a message with expecting an answer.
+			
+			message = URLDecoder.decode(message, "UTF8");
+			
+			Question q = new Question(questionNo, message, type);
+			
+			if(parts.length==1) {
+				
+				result = QuestionBuilder.build(q,servletURL);
+			}
+		}
+		
+		resp.getWriter().print(result);
+	}
+}
